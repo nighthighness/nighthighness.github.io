12b22ce179675240aa56fb811702fb6959a24fe7
nighthighness <nighthighness@163.com>
2019-08-25 18:11:30 +0800

    Site updated: 2019-08-25 18:11:29

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,17 +1,1 @@
-//崩溃欺骗
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-//         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '不要玩啦,快回来~';
-         clearTimeout(titleTime);
-     }
-     else {
-//         $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '你回来啦~' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?(document.title="不要玩啦,快回来~",clearTimeout(titleTime)):(document.title="你回来啦~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
